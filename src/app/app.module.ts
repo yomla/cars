1858f58c8831bc604ff2375f33400774cfc9ba9e
Mladen <djoml8bre@gmail.com>
2017-08-29 14:42:39 +0200

    Zadatak 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes = [
+  {
+    path: '',
+    redirectTo: '/cars',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'cars',
+    component: CarsComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    CarsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
